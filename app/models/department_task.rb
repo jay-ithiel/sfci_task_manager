6dcfb018465c239fcaec5555f564f9b5da7e891b
Jay Hwang <jayhwang0121@gmail.com>
2017-12-31 23:30:18 -0800

    minor refactor

--- app/models/department_task.rb
+++ app/models/department_task.rb
@@ -1,5 +1,6 @@
 class DepartmentTask < ApplicationRecord
   validates :department_id, :task_id, presence: true
+  
   belongs_to :department
   belongs_to :task
 end
--- app/models/employee_department.rb
+++ app/models/employee_department.rb
@@ -1,5 +1,6 @@
 class EmployeeDepartment < ApplicationRecord
   validates :employee, :department, presence: true
+  
   belongs_to :employee
   belongs_to :department
 end
