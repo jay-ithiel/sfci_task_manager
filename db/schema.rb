62fcc2f94ed03b1a56bcc3e43713152ca8e82773
Jay Hwang <jayhwang0121@gmail.com>
2018-01-01 00:07:17 -0800

    change subtask to sub_task

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101052717) do
+ActiveRecord::Schema.define(version: 20180101074018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "department_sub_tasks", force: :cascade do |t|
+    t.integer  "department_id", null: false
+    t.integer  "sub_task_id",   null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["department_id", "sub_task_id"], name: "index_department_sub_tasks_on_department_id_and_sub_task_id", unique: true, using: :btree
+    t.index ["department_id"], name: "index_department_sub_tasks_on_department_id", using: :btree
+    t.index ["sub_task_id"], name: "index_department_sub_tasks_on_sub_task_id", using: :btree
+  end
 
   create_table "department_tasks", force: :cascade do |t|
     t.integer  "department_id", null: false
@@ -47,6 +57,18 @@ ActiveRecord::Schema.define(version: 20180101052717) do
     t.datetime "updated_at",      null: false
     t.index ["email"], name: "index_employees_on_email", using: :btree
     t.index ["username"], name: "index_employees_on_username", using: :btree
+  end
+
+  create_table "sub_tasks", force: :cascade do |t|
+    t.string   "name",                         null: false
+    t.text     "description",                  null: false
+    t.integer  "task_id",                      null: false
+    t.integer  "assignee_id",                  null: false
+    t.integer  "assigner_id",                  null: false
+    t.datetime "due_date",                     null: false
+    t.boolean  "is_completed", default: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "tasks", force: :cascade do |t|
