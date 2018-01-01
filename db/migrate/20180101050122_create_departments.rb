b1a3a64211f5c5f4c49a8d48850c37bf4f3c908d
Jay Hwang <jayhwang0121@gmail.com>
2017-12-31 21:02:22 -0800

    create departments model & table

---
+++ db/migrate/20180101050122_create_departments.rb
@@ -0,0 +1,9 @@
+class CreateDepartments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :departments do |t|
+      t.string :name, null: false, index: true, unique: true
+
+      t.timestamps
+    end
+  end
+end
