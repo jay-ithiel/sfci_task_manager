6a5bec56b0245d5c8dc7231f551c26c3cac81487
Jay Hwang <jayhwang0121@gmail.com>
2018-01-02 13:00:44 -0800

    fill out tasks & sub_tasks controllers

--- app/controllers/api/sub_tasks_controller.rb
+++ app/controllers/api/sub_tasks_controller.rb
@@ -1,2 +1,51 @@
 class Api::SubTasksController < ApplicationController
+  def create
+    @sub_task = SubTask.new(sub_task_params)
+    if @sub_task.save
+      render :show
+    else
+      render json: @sub_task.errors.fulL_messages, status: 422
+    end
+  end
+
+  def update
+    @sub_task = SubTask.new(sub_task_params)
+    if @sub_task.save
+      render :show
+    else
+      render json: @sub_task.errors.fulL_messages, status: 422
+    end
+  end
+
+  def show
+    @sub_task = SubTask.find(params[:id])
+    render :show
+  end
+
+  def index
+    @sub_tasks = SubTask.all
+    render :index
+  end
+
+  def destroy
+    @sub_task = SubTask.find(params[:id])
+    @sub_task.destroy
+    render json: {}
+  end
+
+  private
+
+    def sub_task_params
+      params.require(:sub_task).permit(
+        :name,
+        :description,
+        :assignee_id,
+        :assigner_id,
+        :due_date,
+        :departments,
+        :task,
+        :assignee,
+        :assigner
+      )
+    end
 end
--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,51 @@
 class Api::TasksController < ApplicationController
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render :show
+    else
+      render json: @task.errors.fulL_messages, status: 422
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update
+      render :show
+    else
+      render json: @task.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render :show
+  end
+
+  def index
+    @tasks = Task.all
+    render :index
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    render json: {}
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(
+        :name,
+        :description,
+        :assignee_id,
+        :assigner_id,
+        :due_date,
+        :sub_tasks,
+        :departments
+        :assigner,
+        :assignee
+      )
+    end
 end
