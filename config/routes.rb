e14815562da866c40c5c1e8f8de1cd74858e7d63
Jay Hwang <jayhwang0121@gmail.com>
2018-01-02 11:25:28 -0800

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#root"
+
+  namespace :api, defaults: { format: :json } do
+    resources :employees, only: [:create, :update, :show, :index, :destroy]
+    resource :session, only: [:create, :destroy]
+    resources :departments, only [:create, :update, :show, :index, :destroy]
+    resources :tasks, only [:create, :update, :show, :index, :destroy]
+    resources :sub_tasks, only [:create, :update, :show, :index, :destroy]
+  end
 end
+#
+# Rails.application.routes.draw do
+#   root to: "static_pages#root"
+#
+#   namespace :api, defaults: { format: :json } do
+#     resources :users, only: [:create, :home]
+#     resource :session, only: [:create, :destroy]
+#     resources :spots, only: [:create, :update, :show, :index, :destroy]
+#     resources :bookings, only: [:create, :index, :show, :destroy] do
+#       patch 'approve'
+#       patch 'deny'
+#     end
+#     resources :reviews
+#   end
+# end
