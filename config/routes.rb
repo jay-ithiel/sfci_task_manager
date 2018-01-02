06da8220e4e472beaf562a0724c6e64f2360abe7
Jay Hwang <jayhwang0121@gmail.com>
2018-01-02 13:00:59 -0800

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,3 @@ Rails.application.routes.draw do
     resources :sub_tasks, only [:create, :update, :show, :index, :destroy]
   end
 end
-#
-# Rails.application.routes.draw do
-#   root to: "static_pages#root"
-#
-#   namespace :api, defaults: { format: :json } do
-#     resources :users, only: [:create, :home]
-#     resource :session, only: [:create, :destroy]
-#     resources :spots, only: [:create, :update, :show, :index, :destroy]
-#     resources :bookings, only: [:create, :index, :show, :destroy] do
-#       patch 'approve'
-#       patch 'deny'
-#     end
-#     resources :reviews
-#   end
-# end
